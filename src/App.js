4d087befd1560164bf9cbe175f5573fddc2209c3
ervinask <klimavicius.ervinas@gmail.com>
2022-06-01 18:40:27 +0300

    button component added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "normalize.css";
 
 import Hero from "./components/Hero/Hero";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div>
       <Hero title="Title" subtitle="This is subtitle..." />
+      <Button children={"ate"} type="button" />
     </div>
   );
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Button.css";
+
+const Button = ({ type, children }) => {
+  return <button>{children}</button>;
+};
+
+export default Button;
