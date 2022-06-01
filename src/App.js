cf4d27648d486e3e3799ce076b05bfeb9f4f1297
ervinask <klimavicius.ervinas@gmail.com>
2022-06-01 18:52:57 +0300

    section component added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import "normalize.css";
 
 import Hero from "./components/Hero/Hero";
 import Button from "./components/Button/Button";
+import Section from "./components/Section/Section";
 
 function App() {
   return (
     <div>
       <Hero title="Title" subtitle="This is subtitle..." />
-      <Button type="button" color="bold">
-        Ate
-      </Button>
+      <Section>
+        <Button type="button" color="bold">
+          Ate
+        </Button>
+        <Button type="button" color="outline">
+          Labas
+        </Button>
+      </Section>
     </div>
   );
 }
